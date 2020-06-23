b4536399ea110a44f8e0a13fc14039973c9e5663
paddyjclancy <patrickclancy0605@gmail.com>
2020-06-23 20:21:53 +0100

    Lab complete, commited to new repo

---
+++ starter-code/environment/db/provision.sh
@@ -0,0 +1,23 @@
+
+
+# Public Key
+wget -qO - https://www.mongodb.org/static/pgp/server-3.2.asc | sudo apt-key add -
+
+
+# Generate file with mongodb repo url
+echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
+# Refresh local database with packages
+sudo apt-get update
+# Install specific version of MongoDB
+sudo apt-get install -y mongodb-org=3.2.20 mongodb-org-server=3.2.20 mongodb-org-shell=3.2.20 mongodb-org-mongos=3.2.20 mongodb-org-tools=3.2.20
+
+
+sudo service mongod start
+sudo systemctl enable mongod.service
+
+# # Prevent updates
+# echo "mongodb-org hold" | sudo dpkg --set-selections
+# echo "mongodb-org-server hold" | sudo dpkg --set-selections
+# echo "mongodb-org-shell hold" | sudo dpkg --set-selections
+# echo "mongodb-org-mongos hold" | sudo dpkg --set-selections
+# echo "mongodb-org-tools hold" | sudo dpkg --set-selections
